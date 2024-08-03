48fcca1db35858161604f117389b514a09196959
nayan0210git config --global user.email nayanpandey2001@gmail.com <nayanpandey2001@gmail.com>
2024-08-03 19:42:57 +0530

    Add project files.

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Class1
+    {
+        static void Main(String[] args)
+        {
+           /* byte a = 50;
+            Console.WriteLine(a);
+            Console.WriteLine(int.MaxValue);
+            int x = 50;
+            int y = 80;
+            bool z = x>y;
+            Console.WriteLine(z);*/
+
+            float a = 53.4565151556111f;
+            double b = 54.8465145181485685554d;
+            decimal c = 15478325842.5151515151451554145m;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
